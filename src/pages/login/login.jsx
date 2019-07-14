e806f6ba72192f808b4913cedcd405f7a8efd549
MarlonChiu <knight1898@outlook.com>
2019-07-14 13:49:28 +0800

    login 页面布局

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,12 +1,58 @@
 import React, { Component } from 'react'
+import logo from '../../assets/images/logo.png'
+import './login.less'
+import {
+    Form,
+    Input,
+    Icon,
+    Button,
+    } from 'antd'
 
 // 用户登陆的路由组件
 class Login extends Component {
+    constructor(props) {
+        super(props)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
     render() { 
         return ( 
-            <div>Login </div>
-         );
+            <div className='login'>
+                <header className='login-header'>
+                    <img src={logo} alt="logo"/>
+                    <h1>React 项目: 后台管理系统</h1>
+                </header>
+                <section className='login-content'>
+                    <h3>用户登陆</h3>
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <Form.Item>
+                            <Input
+                            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            placeholder="Username"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Input
+                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                type="password"
+                                placeholder="Password"
+                                />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                            登录
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </section>
+            </div>
+         )
+    }
+
+    handleSubmit(e) {
+        // 阻止事件默认行为(不提交表单)
+        e.preventDefault()
+        console.log(e)
     }
 }
  
-export default Login;
+export default Login
