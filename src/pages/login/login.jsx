f84421024f8ef5845a7696891719c1466d042fe5
marlonchiu <Knight1898@outlook.com>
2019-07-15 12:54:00 +0800

    login form 收集表单单数据

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -8,13 +8,19 @@ import {
     Button,
     } from 'antd'
 
+// const Item = Form.Item // 不能写在import 之前
+
 // 用户登陆的路由组件
 class Login extends Component {
     constructor(props) {
         super(props)
         this.handleSubmit = this.handleSubmit.bind(this)
     }
-    render() { 
+
+    render() {
+        // this.props.form 得到具备强大功能的form 对象
+        const { getFieldDecorator } = this.props.form
+
         return ( 
             <div className='login'>
                 <header className='login-header'>
@@ -25,17 +31,35 @@ class Login extends Component {
                     <h3>用户登陆</h3>
                     <Form onSubmit={this.handleSubmit} className="login-form">
                         <Form.Item>
-                            <Input
-                            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                            placeholder="Username"
-                            />
+                          {
+                              getFieldDecorator('username', {
+                                rules: [{ required: true, message: 'Please input your username!' }],
+                              })(
+                                <Input
+                                  prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                  placeholder="Username"
+                                />
+                              )
+                          }
                         </Form.Item>
                         <Form.Item>
-                            <Input
-                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                type="password"
-                                placeholder="Password"
-                                />
+                          {
+                              getFieldDecorator('PASSWORD', {
+                                rules: [{ required: true, message: 'Please input your Password!' }],
+                              })(
+                                <Input
+                                  prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                  type="password"
+                                  placeholder="Password"
+                                />,
+                              )
+                          }
+
+                            {/*<Input*/}
+                                {/*prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}*/}
+                                {/*type="password"*/}
+                                {/*placeholder="Password"*/}
+                                {/*/>*/}
                         </Form.Item>
                         <Form.Item>
                             <Button type="primary" htmlType="submit" className="login-form-button">
@@ -51,8 +75,30 @@ class Login extends Component {
     handleSubmit(e) {
         // 阻止事件默认行为(不提交表单)
         e.preventDefault()
-        console.log(e)
+        const form = this.props.form
+        const values = form.getFieldsValue()
+        console.log(values)
     }
 }
- 
-export default Login
+
+/*
+* 高阶函数
+*   Form.create()
+* 高阶组件
+*   Form.create() 包装Form组件生成一个新的组件Form(Login) 可以通过react dev tools 查看
+*   新组件会向Form组件传递一个强大的对象属性 form
+* */
+// export default Login
+const WrappedLoginForm = Form.create({ name: 'normal_login' })(Login)
+export default WrappedLoginForm
+
+/*
+    前台表单验证，收据收集
+
+    用户名/密码的合法性要求：
+        用户名/密码的的合法性要求
+        1). 必须输入
+        2). 必须大于等于 4 位
+        3). 必须小于等于 12 位
+        4). 必须是英文、 数字或下划线组成
+ */
