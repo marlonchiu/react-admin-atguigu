1c3dd37a4847f2f878c018160d8ed502da1f1d8b
marlonchiu <Knight1898@outlook.com>
2019-07-15 14:11:41 +0800

    login form 表单验证

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,12 +1,7 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import logo from '../../assets/images/logo.png'
 import './login.less'
-import {
-    Form,
-    Input,
-    Icon,
-    Button,
-    } from 'antd'
+import {Button, Form, Icon, Input,} from 'antd'
 
 // const Item = Form.Item // 不能写在import 之前
 
@@ -16,6 +11,36 @@ class Login extends Component {
         super(props)
         this.handleSubmit = this.handleSubmit.bind(this)
     }
+
+   /*
+        对密码进行自定义验证
+         用户名/密码的的合法性要求
+             1). 必须输入
+             2). 必须大于等于4位
+             3). 必须小于等于12位
+             4). 必须是英文、数字或下划线组成
+    */
+    validatePwd = (rule, value, callback) => {
+        // console.log(value, rule)
+        const pwdLen = value && value.length
+        const pwdReg = /^[a-zA-Z0-9_]+$/
+        if (!value) {
+            callback('必须输入密码')
+        } else if (pwdLen < 4) {
+            callback('密码必须大于4位')
+        } else if (pwdLen > 12) {
+            callback('密码必须小于 12 位')
+        } else if (!pwdReg.test(value)) {
+            callback('密码必须是英文、 数组或下划线组成')
+        } else {
+            callback() // 必须调用 callback
+        }
+
+        // 语法
+        // callback() // 验证通过
+        // callback('xxxx') // 验证失败, 并指定提示的文本
+    }
+  
 
     render() {
         // this.props.form 得到具备强大功能的form 对象
@@ -31,9 +56,24 @@ class Login extends Component {
                     <h3>用户登陆</h3>
                     <Form onSubmit={this.handleSubmit} className="login-form">
                         <Form.Item>
+                            {/*
+
+                                 用户名/密码的的合法性要求
+                                     1). 必须输入
+                                     2). 必须大于等于4位
+                                     3). 必须小于等于12位
+                                     4). 必须是英文、数字或下划线组成
+
+                            */}
                           {
-                              getFieldDecorator('username', {
-                                rules: [{ required: true, message: 'Please input your username!' }],
+                              getFieldDecorator('username', {   // 配置对象: 属性名是特定的一些名称
+                                // 声明式验证: 直接使用别人定义好的验证规则进行验证
+                                rules: [
+                                  { required: true, whitespace: true, message: '用户名必须输入!' },
+                                  { min: 4, message: '用户名至少4位' },
+                                  { max: 12, message: '用户名最多12位' },
+                                  { pattern: /^[a-zA-Z0-9_]+$/, message: '用户名必须是英文、数字或下划线组成' }
+                                  ],
                               })(
                                 <Input
                                   prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
@@ -45,7 +85,11 @@ class Login extends Component {
                         <Form.Item>
                           {
                               getFieldDecorator('PASSWORD', {
-                                rules: [{ required: true, message: 'Please input your Password!' }],
+                                rules: [
+                                  { required: true, message: 'Please input your Password!' },
+                                  // 使用自定义的验证规则
+                                  { validator: this.validatePwd }
+                                  ],
                               })(
                                 <Input
                                   prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
@@ -75,9 +119,15 @@ class Login extends Component {
     handleSubmit(e) {
         // 阻止事件默认行为(不提交表单)
         e.preventDefault()
-        const form = this.props.form
-        const values = form.getFieldsValue()
-        console.log(values)
+        // const form = this.props.form
+        // const values = form.getFieldsValue()
+        // console.log(values)
+
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
     }
 }
 
