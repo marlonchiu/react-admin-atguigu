988ed2d023c8c28b849b2a7b6c935e8363d12f34
marlonchiu <Knight1898@outlook.com>
2019-07-16 11:02:29 +0800

    admin header头部静态布布局

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -5,7 +5,20 @@ class Header extends Component {
 
     render() { 
         return ( 
-            <div className='header'>header</div>
+            <div className='header'>
+                <div className="header-top">
+                    <span>欢迎, admin</span>
+                    <a href='javascrpt;'>退出</a>
+                </div>
+                <div className='header-bottom'>
+                    <div className='header-bottom-left'>首页</div>
+                    <div className='header-bottom-right'>
+                        <span>2019-07-16 10:52</span>
+                        <img src="http://api.map.baidu.com/images/weather/day/qing.png" alt="weather"/>
+                        <span>晴</span>
+                    </div>
+                </div>
+            </div>
          );
     }
 }
