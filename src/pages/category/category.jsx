f25caf85259d2f85bcb3504b919b6c7a7501ae88
marlonchiu <Knight1898@outlook.com>
2019-07-16 16:09:32 +0800

    category 静态布局一

--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -1,9 +1,68 @@
 import React, { Component } from 'react'
+import { Card, Table, Button, Icon} from 'antd'
+import LinkButton from '../../components/link-button'
+
 class Category extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div>Category</div>
+
+    render() {
+        // card 左侧标题
+        const title = '一级分类标题'
+        // card 右侧
+        const extra = (
+            <Button type="primary">
+                <Icon type="plus"/>
+                增加
+            </Button>
+
+        )
+        const dataSource = [
+            {
+                "parentId": "0",
+                "_id": '5ca9dffsafsa',
+                "name": "家用电器",
+                "_v": 0
+            },
+            {
+                "parentId": "0",
+                "_id": '5ca9dffsdfasdfsvcs',
+                "name": "电脑",
+                "_v": 0
+            },
+            {
+                "parentId": "0",
+                "_id": '5ca9dffesfwefwe',
+                "name": "手机",
+                "_v": 0
+            }
+        ];
+
+        const columns = [
+            {
+                title: '分类名称',
+                dataIndex: 'name' // 显示数据对应的属性名
+            },
+            {
+                title: '操作',
+                width: 300,
+                align: 'center',
+                render: () => ( // 返回需要显示的界面标签
+                    <span>
+                        <LinkButton>修改分类</LinkButton>
+                        <LinkButton>查看子分类</LinkButton>
+                    </span>
+                )
+            },
+        ];
+
+
+        return (
+            <Card title={title} extra={extra}>
+                <Table
+                    bordered
+                    rowKey='_id'
+                    dataSource={dataSource}
+                    columns={columns} />
+            </Card>
          );
     }
 }
