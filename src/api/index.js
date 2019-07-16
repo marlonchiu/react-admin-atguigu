07c6497dad69175dd821490bffd77aab37efeffb
marlonchiu <Knight1898@outlook.com>
2019-07-16 13:58:04 +0800

    admin header头部获取天气数据并显示 jsonp请求的理解

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 // 包含应用中所有的请求接口
 
 import ajax from './ajax'
+import jsonp from 'jsonp'
+import {message} from 'antd'
 
 // const BASE_URL = 'http://localhost:5000'
 const BASE_URL = ''
@@ -10,3 +12,38 @@ export const reqLogin = (username, password)=> ajax(BASE_URL + '/login', {userna
 
 // 添加用户
 export const reqAddUser = (user) => ajax(BASE_URL + '/manage/user/add', user, 'POST')
+
+// 百度天气
+export const reqWeather = (city) => {
+    const url = `http://api.map.baidu.com/telematics/v3/weather?location=${city}&ou
+tput=json&ak=3p49MVra6urFRGOT9s8UBWr2`
+    return new Promise(((resolve, reject) => {
+        jsonp(url, {
+            params: 'callback'
+        }, (error, data) => {
+            // console.log(data);
+            if(!error && data.status === 'success') {
+                const {dayPictureUrl, weather} = data.results[0].weather_data[0]
+                resolve({dayPictureUrl, weather})
+            } else {
+                message.error('获取天气信息失败!')
+            }
+        })
+
+    }))
+}
+
+// reqWeather('北京')
+/*
+    jsonp解决ajax跨域的原理
+      1). jsonp只能解决GET类型的ajax请求跨域问题
+      2). jsonp请求不是ajax请求, 而是一般的get请求
+      3). 基本原理
+       浏览器端:
+          动态生成<script>来请求后台接口(src就是接口的url)
+          定义好用于接收响应数据的函数(fn), 并将函数名通过请求参数提交给后台(如: callback=fn  callback=__jp0)
+       服务器端:
+          接收到请求处理产生结果数据后, 返回一个函数调用的js代码, 并将结果数据作为实参传入函数调用
+       浏览器端:
+          收到响应自动执行函数调用的js代码, 也就执行了提前定义好的回调函数, 并得到了需要的结果数据
+ */
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,25 +1,90 @@
 import React, { Component } from 'react'
 import './index.less'
+import {reqWeather} from '../../api'
+import { formateDate } from '../../utils/dateUtils'
+import memoryUtils from '../../utils/memoryUtils'
+import storageUtils from '../../utils/storageUtils'
+import {Modal} from 'antd'
 
 class Header extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            sysTime: formateDate(Date.now()),
+            dayPictureUrl: '', // 天气图片的 url
+            weather: ''
+        }
+    }
 
-    render() { 
+    // 发异步 ajax 获取天气数据并更新状态
+    getWeather = async() => {
+        const {dayPictureUrl, weather} = await reqWeather('北京')
+        this.setState({
+            dayPictureUrl,
+            weather
+        })
+    }
+
+    // 启动循环定时器, 每隔 1s 更新一次 sysTime
+    getSysTime = () => {
+        this.intervalId = setInterval(() => {
+            this.setState({
+                sysTime: formateDate(Date.now())
+            })
+        }, 1000)
+    }
+
+
+    componentDidMount() {
+        this.getSysTime()
+        this.getWeather()
+    }
+
+    componentWillUnmount() {
+        // 清除定时器
+        clearInterval(this.intervalId)
+    }
+
+
+    render() {
+        // 数据解构赋值
+        const {sysTime, dayPictureUrl, weather} = this.state
+        // 当前登录的用户
+        const user = memoryUtils.user
+
         return ( 
             <div className='header'>
                 <div className="header-top">
-                    <span>欢迎, admin</span>
-                    <a href='javascrpt;'>退出</a>
+                    <span>欢迎, {user.username}</span>
+                    <a onClick={this.logout}>退出</a>
                 </div>
                 <div className='header-bottom'>
                     <div className='header-bottom-left'>首页</div>
                     <div className='header-bottom-right'>
-                        <span>2019-07-16 10:52</span>
-                        <img src="http://api.map.baidu.com/images/weather/day/qing.png" alt="weather"/>
-                        <span>晴</span>
+                        <span>{sysTime}</span>
+                        <img src={dayPictureUrl} alt="weather"/>
+                        <span>{weather}</span>
                     </div>
                 </div>
             </div>
          );
+    }
+
+    // 退出登录
+    logout = () => {
+        Modal.confirm({
+            content: '确定退出当前系统吗?',
+            onOk: () => {
+                console.log('ok')
+                // 移除登录信息user 跳转到login
+                storageUtils.removeUser()
+                memoryUtils.user = {}
+                this.props.history.replace('/login')
+            },
+            onCancel() {
+                console.log('cancel')
+            }
+        });
     }
 }
  
