956083f1a9852774e07df5958ae55c9da341e6bb
marlonchiu <Knight1898@outlook.com>
2019-07-17 21:38:54 +0800

    product-add-update 商品分类静态介绍

--- src/pages/product/product-add-update.jsx
+++ src/pages/product/product-add-update.jsx
@@ -12,8 +12,56 @@ import {
 const { Item } = Form
 const { TextArea } = Input
 
-class ProductAddUpdate extends Component {
+const options = [
+    {
+        value: 'zhejiang',
+        label: 'Zhejiang',
+        isLeaf: false,
+    },
+    {
+        value: 'jiangsu',
+        label: 'Jiangsu',
+        isLeaf: false,
+    }
+]
 
+class ProductAddUpdate extends Component {
+    state = {
+        options,
+    }
+
+    onChange = (value, selectedOptions) => {
+        console.log(value, selectedOptions);
+    }
+
+    /*
+        用加载下一级列表的回调函数
+   */
+    loadData = selectedOptions => {
+        // 得到选择的option对象
+        const targetOption = selectedOptions[selectedOptions.length - 1];
+
+        // 显示loading效果
+        targetOption.loading = true;
+
+        // load options lazily
+        setTimeout(() => {
+            targetOption.loading = false;
+            targetOption.children = [
+                {
+                    label: `${targetOption.label} Dynamic 1`,
+                    value: 'dynamic1',
+                },
+                {
+                    label: `${targetOption.label} Dynamic 2`,
+                    value: 'dynamic2',
+                },
+            ];
+            this.setState({
+                options: [...this.state.options],
+            });
+        }, 1000);
+    };
 
     // 验证价格的自定义验证函数
     validatePrice = (rule, value, callback) => {
@@ -83,8 +131,15 @@ class ProductAddUpdate extends Component {
                             })(<Input type='number' placeholder='请输入商品价格' addonAfter='元'/>)
                         }
                     </Item>
+
                     <Item label="商品分类">
-                        <div>商品分类</div>
+                        <Cascader
+                            placeholder='请指定商品分类'
+                            options={this.state.options}  /*需要显示的列表数据数组*/
+                            loadData={this.loadData}  /*当选择某个列表项, 加载下一级列表的监听回调*/
+                            onChange={this.onChange}
+                            changeOnSelect
+                        />
                     </Item>
                     <Item label="商品图片">
                         <div>商品图片</div>
@@ -105,6 +160,8 @@ class ProductAddUpdate extends Component {
             if(!error) {
                 // 发送请求
                 console.log(values)
+
+                message.error('数据获取失败')
             }
         })
     }
