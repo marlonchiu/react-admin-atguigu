9cfb342efd1af19d9a0dc9a747fe4da98f49a552
marlonchiu <Knight1898@outlook.com>
2019-07-17 22:19:00 +0800

    product-add-update 商品分类 级联显示二级列表 重要的  视频73

--- src/pages/product/product-add-update.jsx
+++ src/pages/product/product-add-update.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import LinkButton from '../../components/link-button'
+import { reqCategorys } from '../../api'
 import {
     Card,
     Icon,
@@ -12,56 +13,77 @@ import {
 const { Item } = Form
 const { TextArea } = Input
 
-const options = [
-    {
-        value: 'zhejiang',
-        label: 'Zhejiang',
-        isLeaf: false,
-    },
-    {
-        value: 'jiangsu',
-        label: 'Jiangsu',
-        isLeaf: false,
-    }
-]
-
 class ProductAddUpdate extends Component {
     state = {
-        options,
+        options: [],
     }
 
-    onChange = (value, selectedOptions) => {
-        console.log(value, selectedOptions);
+    // 处理options 的通用方法
+    initOptions = (categorys) => {
+        // 根据categorys生成options数组
+        const options = categorys.map(c => ({
+            value: c._id,
+            label: c.name,
+            isLeaf: false, // 不是叶子
+        }))
+
+        // 更新options状态
+        this.setState({
+            options
+        })
+    }
+
+    // 异步获取 一级/ 二级分类列表
+    //  async函数的返回值是一个新的promise对象, promise的结果和值由async的结果来决定
+    getCategorys = async (parentId) => {
+        const result = await reqCategorys(parentId)
+        // debugger
+        if(result.status === 0) {
+            const categorys = result.data
+            // 如果是一级列表
+            if(parentId === '0') {
+                this.initOptions(categorys)
+            } else { // 二级列表
+                return categorys  // 返回二级列表 ==> 当前async函数返回的promsie就会成功且value为categorys
+            }
+        }
     }
 
     /*
         用加载下一级列表的回调函数
    */
-    loadData = selectedOptions => {
+    loadData = async selectedOptions => {
         // 得到选择的option对象
         const targetOption = selectedOptions[selectedOptions.length - 1];
-
+        console.log(targetOption)
         // 显示loading效果
-        targetOption.loading = true;
+        targetOption.loading = true
 
-        // load options lazily
-        setTimeout(() => {
-            targetOption.loading = false;
-            targetOption.children = [
-                {
-                    label: `${targetOption.label} Dynamic 1`,
-                    value: 'dynamic1',
-                },
-                {
-                    label: `${targetOption.label} Dynamic 2`,
-                    value: 'dynamic2',
-                },
-            ];
-            this.setState({
-                options: [...this.state.options],
-            });
-        }, 1000);
-    };
+        // 根据选中的分类，请求获取二级分类列表
+        const subCategorys = await this.getCategorys(targetOption.value)
+
+        targetOption.loading = false
+
+        // 判断二级分类列表 是否 有数据
+        if (subCategorys && subCategorys.length > 0) {
+            // 生成一个二级列表的 options
+            const childrenOptions = subCategorys.map(sub => ({
+                value: sub._id,
+                label: sub.name,
+                isLeaf: true
+            }))
+            // 关联到当前选中的option 上
+            targetOption.children = childrenOptions
+
+        } else { // 当前选中的分类没有二级分类
+            targetOption.isLeaf = true
+        }
+
+        // 更新 options 状态
+        this.setState({
+            options: [...this.state.options]
+        })
+    }
 
     // 验证价格的自定义验证函数
     validatePrice = (rule, value, callback) => {
@@ -71,6 +93,11 @@ class ProductAddUpdate extends Component {
         }else {
             callback('价格必须大于0') // 验证没通过
         }
+    }
+
+    componentDidMount() {
+        // 获取分类列表  初始化时一级的
+        this.getCategorys('0')
     }
 
 
@@ -137,7 +164,6 @@ class ProductAddUpdate extends Component {
                             placeholder='请指定商品分类'
                             options={this.state.options}  /*需要显示的列表数据数组*/
                             loadData={this.loadData}  /*当选择某个列表项, 加载下一级列表的监听回调*/
-                            onChange={this.onChange}
                             changeOnSelect
                         />
                     </Item>
