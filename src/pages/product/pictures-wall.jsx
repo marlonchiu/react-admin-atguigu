3fedcedc18033f2d86631f0a7cb09b477754dc8c
MarlonChiu <knight1898@outlook.com>
2019-07-20 21:47:18 +0800

    pictures-wall 图片上传功能

--- src/pages/product/pictures-wall.jsx
+++ src/pages/product/pictures-wall.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Upload, Icon, Modal } from 'antd';
+import { Upload, Icon, Modal, message } from 'antd';
 
 function getBase64(file) {
     return new Promise((resolve, reject) => {
@@ -48,6 +48,21 @@ class PicturesWall extends React.Component {
      */
     handleChange = ({ file, fileList }) => {
         console.log(file, fileList);
+        // 一旦上传成功, 将当前上传的file的信息修正(name, url)
+        if(file.status === 'done') {
+            const result = file.response  // {status: 0, data: {name: 'xxx.jpg', url: '图片地址'}}
+            if(result.status === 0) {
+                message.success('上传图片成功!')
+                const { name, url } = result.data
+                // file 和 fileList 最后一个不是同一个对象
+                file = fileList[fileList.length - 1]
+                file.name = name
+                file.url = url
+            } else {
+                message.error('上传图片失败')
+            }
+        }
+
         // 在操作(上传/删除)过程中更新fileList状态
         this.setState({ fileList })
     };
