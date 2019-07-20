8136333494c3c070ef1eaad1a8aef51bba3ffa05
MarlonChiu <knight1898@outlook.com>
2019-07-20 23:44:50 +0800

    rich-text-editor 富文本编辑器添加图片

--- src/pages/product/rich-text-editor.jsx
+++ src/pages/product/rich-text-editor.jsx
@@ -50,6 +50,27 @@ class RichTextEditor extends Component {
         return draftToHtml(convertToRaw(this.state.editorState.getCurrentContent()))
     }
 
+    uploadImageCallBack = (file) => {
+        return new Promise(
+            (resolve, reject) => {
+                const xhr = new XMLHttpRequest()
+                xhr.open('POST', '/manage/img/upload')
+                const data = new FormData()
+                data.append('image', file)
+                xhr.send(data)
+                xhr.addEventListener('load', () => {
+                    const response = JSON.parse(xhr.responseText)
+                    const url = response.data.url // 得到图片的url
+                    resolve({data: {link: url}})
+                })
+                xhr.addEventListener('error', () => {
+                    const error = JSON.parse(xhr.responseText)
+                    reject(error)
+                })
+            }
+        )
+    }
+
 
     render() {
         const { editorState } = this.state
@@ -58,6 +79,12 @@ class RichTextEditor extends Component {
                 <Editor
                     editorState={editorState}
                     editorStyle={{border: '1px solid black', minHeight: 200, paddingLeft: 10}}
+                    toolbar={{
+                        image: {
+                            uploadCallback: this.uploadImageCallBack,
+                            alt: { present: true, mandatory: true }
+                        },
+                    }}
                     onEditorStateChange={this.onEditorStateChange}
                 />
                 {/*<textarea*/}
