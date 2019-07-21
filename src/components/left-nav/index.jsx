64b00e554db73a02067041bc254be09c2aea35fd
MarlonChiu <knight1898@outlook.com>
2019-07-21 22:58:23 +0800

    权限列表动态显示匹配

--- src/components/left-nav/index.jsx
+++ src/components/left-nav/index.jsx
@@ -3,7 +3,7 @@ import { Link, withRouter} from 'react-router-dom';
 import './index.less'
 import logo from '../../assets/images/logo.png'
 import menuList from '../../config/menuConfig'
-
+import memoryUtils from "../../utils/memoryUtils"
 import { Menu, Icon } from 'antd'
 const { SubMenu } = Menu
 
@@ -74,6 +74,29 @@ class LeftNav extends Component {
     }
 
     /*
+    *   判断当前登陆用户对item是否有权限
+    * */
+
+    hasAuth = (item) => {
+        const { key, isPublic } = item
+
+        const menus = memoryUtils.user.role.menus
+        const username = memoryUtils.user.username
+        /*
+            1. 如果当前用户是admin
+            2. 如果当前item是公开的
+            3. 当前用户有此item的权限: key有没有menus中
+         */
+        if(username === 'admin' || isPublic || menus.indexOf(key) !== -1) {
+             return true
+        } else if(item.children) { // 4. 如果当前用户有此item的某个子item的权限
+             return  !!item.children.find(child => menus.indexOf(child.key) !== -1)
+        }
+        
+        return false
+    }
+
+    /*
       根据menu的数据数组生成对应的标签数组
       使用reduce() + 递归调用
         相当于向一个空数组中添加数组
@@ -83,40 +106,44 @@ class LeftNav extends Component {
         const path = this.props.location.pathname
 
         return menuList.reduce((pre, item) => {
-            // 向pre添加<Menu.Item>
-            if(!item.children) {
-                pre.push((
-                    <Menu.Item key={item.key}>
-                        <Link to={item.key}>
-                            <Icon type={item.icon} />
-                            <span>{item.title}</span>
-                        </Link>
-                    </Menu.Item>
-                ))
-            } else {
-                // 查找一个与当前请求路径匹配的子Item
-                // const cItem = item.children.find(cItem => cItem.key === path)
-                //      这里采用indexOf处理 为了解决商品页面如果进入详情不选中的bug
-                const cItem = item.children.find(cItem => path.indexOf(cItem.key) === 0)
-                // 如果存在, 说明当前item的子列表需要打开
-                if (cItem) {
-                    this.openKey = item.key
-                }
+            // 如果当前用户有item对应的权限, 才需要显示对应的菜单项
+            // 引入权限验证
+            if(this.hasAuth(item)) {
+                // 向pre添加<Menu.Item>
+                if(!item.children) {
+                    pre.push((
+                        <Menu.Item key={item.key}>
+                            <Link to={item.key}>
+                                <Icon type={item.icon} />
+                                <span>{item.title}</span>
+                            </Link>
+                        </Menu.Item>
+                    ))
+                } else {
+                    // 查找一个与当前请求路径匹配的子Item
+                    // const cItem = item.children.find(cItem => cItem.key === path)
+                    //      这里采用indexOf处理 为了解决商品页面如果进入详情不选中的bug
+                    const cItem = item.children.find(cItem => path.indexOf(cItem.key) === 0)
+                    // 如果存在, 说明当前item的子列表需要打开
+                    if (cItem) {
+                        this.openKey = item.key
+                    }
 
-                // 向pre添加<SubMenu>
-                pre.push((
-                    <SubMenu
-                        key={item.key}
-                        title={
-                            <span>
+                    // 向pre添加<SubMenu>
+                    pre.push((
+                        <SubMenu
+                            key={item.key}
+                            title={
+                                <span>
                       <Icon type={item.icon}/>
                       <span>{item.title}</span>
                     </span>
-                        }
-                    >
-                        {this.getMenuNodes_reduce(item.children)}
-                    </SubMenu>
-                ))
+                            }
+                        >
+                            {this.getMenuNodes_reduce(item.children)}
+                        </SubMenu>
+                    ))
+                }
             }
 
             return pre
