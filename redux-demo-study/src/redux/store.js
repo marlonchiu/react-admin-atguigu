e758f39372606138521f86809346d845a5fc3c53
MarlonChiu <knight1898@outlook.com>
2019-07-23 01:08:13 +0800

    redux 扩展程序使用及配置

--- redux-demo-study/src/redux/store.js
+++ redux-demo-study/src/redux/store.js
@@ -2,9 +2,10 @@
 import { createStore, applyMiddleware } from 'redux'
 import reducer from './reducer'
 import thunk from 'redux-thunk' // 用来实现 redux 异步的 redux 中间件插件
+import {composeWithDevTools} from 'redux-devtools-extension'
 const store = createStore(
   reducer,
-  applyMiddleware(thunk)
+  composeWithDevTools(applyMiddleware(thunk))
 )
 
 export default store   // 创建store对象内部会第一次调用reducer()得到初始状态值
