44875cb751271f1d7c8b2bc079f335f9e9b80123
MarlonChiu <knight1898@outlook.com>
2019-07-23 01:26:47 +0800

    redux 学习第一阶段

---
+++ redux-demo-study/05_src_counter_redux-final/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App.jsx'
+import store from './redux/store'
+import { Provider} from 'react-redux'
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>, document.getElementById('root'));
